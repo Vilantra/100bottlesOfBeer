32205327988e56315769738ce16b6e486a7d7863
Vilantra <mua.amelie@gmail.com>
2020-10-11 21:12:23 -0300

    Initial Commit

---
+++ 100bottlesOfBeerTests/_00bottlesOfBeerTests.m
@@ -0,0 +1,37 @@
+//
+//  _00bottlesOfBeerTests.m
+//  100bottlesOfBeerTests
+//
+//  Created by Karen Reyes Caceres on 11-10-20.
+//  Copyright © 2020 Karen Reyes Caceres. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _00bottlesOfBeerTests : XCTestCase
+
+@end
+
+@implementation _00bottlesOfBeerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
