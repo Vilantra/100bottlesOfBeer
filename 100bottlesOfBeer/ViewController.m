7b9635ab8d474333acdefa097629cf27ba8696fd
Vilantra <mua.amelie@gmail.com>
2020-10-11 21:34:34 -0300

    for with objective c

--- 100bottlesOfBeer/ViewController.m
+++ 100bottlesOfBeer/ViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    int bottle = 99;
+    int bottleM = 1;
+    for (int i=0; i<=bottle; i++) {
+        bottleM = bottle - i;
+        NSLog(@"%d bottles of beer on the wall, %d bottles of beer! Take one down, Pass it around, %d bottles of beer on the wall!", bottleM +1, bottleM +1, bottleM );
+    }
+    if(bottleM == 0) {
+        NSLog(@"No more bottles of beer on the wall!");
+    }
 }
 
 
